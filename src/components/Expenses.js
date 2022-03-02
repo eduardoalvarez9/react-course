d83841a0e55f12530de25523bd321a031f12dd79
Jorge Alvarez <jorge.edu17@hotmail.com>
2022-03-01 22:29:04 -0600

    add card component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,24 +1,25 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import Card from "./Card";
 
 
 function Expenses(props) {
-    return (
-        <div className="expenses">
-        <ExpenseItem 
-          tile={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}/>
-        <ExpenseItem 
-          tile={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}/>
-        <ExpenseItem 
-          tile={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}/>
-        </div>
-    );
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        tile={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date} />
+      <ExpenseItem
+        tile={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date} />
+      <ExpenseItem
+        tile={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date} />
+    </Card>
+  );
 }
 
 export default Expenses;
